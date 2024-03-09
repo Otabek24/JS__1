4e9a7f6dc9b491ea6ededa90b47fe970645cc5fa
Otabek <Otabek>
2024-03-09 14:55:44 +0500

    html

---
+++ script.js
@@ -0,0 +1,29 @@
+var name = prompt('Ismingizni kiriting')
+
+console.log('Sizning ismingiz ' + name)
+
+var age = prompt('Yoshingizni kiriting')
+
+console.log('Sizning yoshingiz ' + age)
+
+var question = prompt('6+3=?')
+
+console.log('Sizning javobingiz ' + question + ". To'g'ri javob:6+3=9")
+
+var question2 = prompt('10-3=?')
+
+console.log('Sizning javobingiz ' + question2 + ". To'g'ri javob:10-3=7")
+
+var question3 = prompt('12/3=?')
+
+console.log('Sizning javobingiz ' + question3 + ". To'g'ri javob:12/3=4")
+
+var question4 = prompt('9*9=?')
+
+console.log('Sizning javobingiz ' + question4 + ". To'g'ri javob:9*9=81")
+
+var question5 = prompt('13%2=?')
+
+console.log('Sizning javobingiz ' + question5 + ". To'g'ri javob:13%2=1")
+
+alert('Konsolni ochib tekshirishingiz mumkin')
